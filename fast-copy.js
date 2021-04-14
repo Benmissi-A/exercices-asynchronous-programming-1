28adbcfc25e102f1b5e8aaede22c3748cba8e8bd
Benmissi-A <aminebenmissi@hotmail.fr>
2021-04-14 20:20:02 +0200

    push

--- fast-copy.js
+++ fast-copy.js
@@ -5,21 +5,35 @@ const fsPromises =require('fs/promises')
 const path = process.argv[2]
 const newPath = process.argv[3]
 
+if(process.argv.length !==4){
+  console.log(`only two aruments exepted: path , copy-path`)
+  process.exit()
+}
+if(!fs.existsSync(path)){
+console.log(`source directory doesn't exist`)
+}
 
-
-
-  const addFile = async (src , dest) => {
+const addFile = async (src , dest) => {
+  try{
     await fsPromises.copyFile(src, dest)
+  }catch(e){
+    throw e
   }
+}
 
-  const fastCopy = async (path ,newPath) => {
+const fastCopy = async (path ,newPath) => {
+  try{
     //todo verifier l'existence du nouveau dossier
-    await fsPromises.mkdir(newPath)
-  const dirFiles= await fsPromises.readdir(path)
-  console.log(dirFiles)
-
-  for(let i=0 ; i<dirFiles.length ; ++i){
-  addFile(path+'/'+dirFiles[i],newPath+'/'+dirFiles[i])
+    if(!fs.existsSync(newPath)){
+      await fsPromises.mkdir(newPath)
+    }
+    const dirFiles= await fsPromises.readdir(path)
+    console.log(dirFiles)
+    for(let i=0 ; i<dirFiles.length ; ++i){
+      addFile(path+'/'+dirFiles[i],newPath+'/'+dirFiles[i])
+    }  
+  }catch(e){
+    console.log(e.message)
   }
 }
 
